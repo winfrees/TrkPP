d569214f4b5daafaf760de251970b1a16cb36a7d
winfrees <winfrees@IN-MDEP-120247.ads.iu.edu>
2014-04-08 13:28:44 -0400

    Merge origin/master
    Conflicts:
    	README.md

--- src/main/java/TrkPP/Trk_PP.java
+++ src/main/java/TrkPP/Trk_PP.java
@@ -1,13 +1,42 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2014 Indiana University
+ * Authors email winfrees at iupui dot edu
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
+/**
+ *
+ * @author Seth Winfree <Seth Winfree at Indiana University>
+ */
+
+/**
+ * Trk_PP is a preprocessing plugin for modifying datasets to allow the plugin
+ * Trackmate to efficiently follow cells stained either with poor cell body
+ * delineation, as stained by a fluid phase marker, or extended cells with
+ * multiple processes as with dendritic cells.  The plugin allows for 
+ * significant preprocessing of specific channels from multichannel datasets.
+ * 
+ * General algorithm 1) clean-up cross channel signal by simple image math.
+ *                   2) Dilate cleaned up image.
+ *                   3) Threshold, find object centroids with particle Analyzer.
+ *                   4) Plot centroids per size requirements.
+ */
 package TrkPP;
 
 import ij.IJ;
-import ij.ImageJ;
 import ij.ImagePlus;
 import ij.ImageStack;
 import ij.gui.GenericDialog;
@@ -20,58 +49,8 @@ import ij.plugin.filter.PlugInFilter;
 import ij.process.ImageProcessor;
 
 
+public class Trk_PP implements PlugInFilter {
 
-/**
- *
- * @author winfrees
- */
-public class TrkPP_ implements PlugInFilter {
-
-     public static void main(String[] args) {
-		// set the plugins.dir property to make the plugin appear in the Plugins menu
-		Class<?> clazz = TrkPP_.class;
-		String url = clazz.getResource("/" + clazz.getName().replace('.', '/') + ".class").toString();
-		String pluginsDir = url.substring(5, url.length() - clazz.getName().length() - 6);
-		System.setProperty("plugins.dir", pluginsDir);
-
-		// start ImageJ
-		new ImageJ();
-
-		// run the plugin
-		IJ.runPlugIn(clazz.getName(), "");
-	}
-    
-//    public static void main(String args[]) {
-//        /* Set the Nimbus look and feel */
-//        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-//        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-//         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-//         */
-//        try {
-//            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-//                if ("Nimbus".equals(info.getName())) {
-//                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-//                    break;
-//                }
-//            }
-//        } catch (ClassNotFoundException ex) {
-//            java.util.logging.Logger.getLogger(UI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        } catch (InstantiationException ex) {
-//            java.util.logging.Logger.getLogger(UI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        } catch (IllegalAccessException ex) {
-//            java.util.logging.Logger.getLogger(UI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-//            java.util.logging.Logger.getLogger(UI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        }
-//        //</editor-fold>
-//
-//        /* Create and display the form */
-//        java.awt.EventQueue.invokeLater(new Runnable() {
-//            public void run() {
-//                new UI().setVisible(true);
-//            }
-//        });
-//    }
     
     ImagePlus imp; 
     Object[] start = new Object[9];
@@ -94,8 +73,8 @@ public class TrkPP_ implements PlugInFilter {
       ResultsTable rt1 = new ResultsTable();
       ResultsTable rt2 = new ResultsTable();    
       ImageCalculator ic = new ImageCalculator();  
-      impResult1 = ic.run("Subtract create stack", new ImagePlus("C3" ,stacks[(int)start[0]-1]), new ImagePlus("C1" ,stacks[(int)start[1]-1]));
-      if((start[2].toString().equals(""))){impResult1 = ic.run("Subtract create stack", impResult1, new ImagePlus("C1" ,stacks[(int)start[2]-1]));}
+      impResult1 = ic.run("Subtract create stack", new ImagePlus("C3" ,stacks[(Integer)start[0]-1]), new ImagePlus("C1" ,stacks[(Integer)start[1]-1]));
+      if((start[2].toString().equals(""))){impResult1 = ic.run("Subtract create stack", impResult1, new ImagePlus("C1" ,stacks[(Integer)start[2]-1]));}
       if(start[3].toString().equals("Yes")){IJ.run(impResult1, "Gaussian Blur...", "sigma=2 stack");}
       ImageStack isResult1;
       ImageStack isResult2 = new ImageStack(this.imp.getWidth(), this.imp.getHeight());
@@ -104,11 +83,11 @@ public class TrkPP_ implements PlugInFilter {
       impResult1.show();     
       isResult1 = impResult1.getImageStack();    
       IJ.run("Set Measurements...", "area redirect=None decimal=3");   
-      ParticleAnalyzer maskParticles = new ParticleAnalyzer(ParticleAnalyzer.SHOW_PROGRESS | ParticleAnalyzer.SHOW_MASKS, 0 ,rt1, (int)start[7], (imp.getWidth()*imp.getHeight()));
+      ParticleAnalyzer maskParticles = new ParticleAnalyzer(ParticleAnalyzer.SHOW_PROGRESS | ParticleAnalyzer.SHOW_MASKS, 0 ,rt1, (Integer)start[7], (imp.getWidth()*imp.getHeight()));
       maskParticles.setHideOutputImage(true);   
       for(int n = 1; n <= stacks[2].getSize(); n++){
 		ImageProcessor ipStack = isResult1.getProcessor(n);
-		ipStack.setThreshold((int)start[5], (int)start[6], ImageProcessor.RED_LUT);
+		ipStack.setThreshold((Integer)start[5], (Integer)start[6], ImageProcessor.RED_LUT);
 		maskParticles.analyze(impResult1, ipStack);
 		ImagePlus maskImage1 = maskParticles.getOutputImage();
 		ImageProcessor maskProcessor = maskImage1.getProcessor();	
@@ -117,7 +96,7 @@ public class TrkPP_ implements PlugInFilter {
       impResult2 = new ImagePlus("Pre Center of Mass Measure", isResult2);
       IJ.run(impResult2, "Dilate", "stack");
       IJ.run(impResult2, "Maximum...", "radius=5 stack");
-      for(int c = 1; c <= (int)start[4]; c++){IJ.run(impResult2, "Dilate", "stack");}
+      for(Integer c = 1; c <= (Integer)start[4]; c++){IJ.run(impResult2, "Dilate", "stack");}
       IJ.run(impResult2, "Fill Holes", "stack");
       impResult2.show("Regions grown");    
       ParticleAnalyzer maskCells = new ParticleAnalyzer(ParticleAnalyzer.SHOW_PROGRESS | ParticleAnalyzer.SHOW_MASKS, Measurements.CENTER_OF_MASS | Measurements.SLICE ,rt2, 0, (imp.getWidth()*imp.getHeight()));
@@ -137,14 +116,13 @@ public class TrkPP_ implements PlugInFilter {
       }   
       ImageStack ResultStack = new ImageStack();     
       ResultStack = this.getCenterOfMassImage(rt2, stacks[0], ResultStack); 
-      impResult4 = new ImagePlus("Centroids in channel " + ((int)start[0]), ResultStack);
+      impResult4 = new ImagePlus("Centroids in channel " + ((Integer)start[0]), ResultStack);
       IJ.run(impResult4, "Maximum...", "radius="+start[8]+" stack"); 
       IJ.resetMinAndMax(impResult4);
       impResult4.show();  
     }
     
-    private ImageStack getCenterOfMassImage(ResultsTable rt, ImageStack stackOriginal, ImageStack StackResult){  
-     // IJ.log(rt.getCounter() + "objects found.");    
+    private ImageStack getCenterOfMassImage(ResultsTable rt, ImageStack stackOriginal, ImageStack StackResult){    
       ImagePlus impResult = NewImage.createImage("Centroids", stackOriginal.getWidth(), stackOriginal.getHeight(), stackOriginal.getSize(), 16, NewImage.FILL_BLACK);
       StackResult = impResult.getImageStack();  
      int height = stackOriginal.getSize();
@@ -153,16 +131,12 @@ public class TrkPP_ implements PlugInFilter {
      int YM;
      int slice;   
       short[] workingpixels;  
-      //add specific points
       for(int j = 0; j <= rtsize-1; j++){
-         // IJ.log("center mass: " + (int)rt.getValueAsDouble(rt.getColumnIndex("XM"), j)    + ", " + (int)rt.getValueAsDouble(rt.getColumnIndex("YM"), j)  + ", " + (int)rt.getValueAsDouble(rt.getColumnIndex("Slice"), j));
           XM = (int)rt.getValueAsDouble(rt.getColumnIndex("XM"),j);
           YM = (int)rt.getValueAsDouble(rt.getColumnIndex("YM"),j);
           slice = (int)rt.getValueAsDouble(rt.getColumnIndex("Slice"),j);  
-         
           workingpixels = (short[])StackResult.getPixels(slice);
           workingpixels[PushingPixels.PixelPositionLinearArray.translate(XM, YM, stackOriginal.getWidth(), stackOriginal.getHeight())] = 255;     
-          
           StackResult.setPixels(workingpixels,slice);
       }
       return StackResult;
@@ -176,30 +150,23 @@ public class TrkPP_ implements PlugInFilter {
 			for(int n = m; n <= imp.getStackSize()-1; n += imp.getNChannels()){stacks[m].addSlice(stack.getProcessor(n+1));}
 		}	
 	return stacks;
-} 
-        
+}   
     public void showDialog(){
-
-                       // int[] windowList = WindowManager.getIDList();
                        String[] YesNo = {"Yes", "No"};
-                       String FileInfo = "File: " + this.imp.getTitle() + ", " + this.imp.getNChannels() + " channels, " + this.imp.getNFrames()+ " frames.";
-                      
+                       String FileInfo = "File: " + this.imp.getTitle() + ", " + this.imp.getNChannels() + " channels, " + this.imp.getNFrames()+ " frames.";           
                         String[] Channels = new String[imp.getNChannels()+1];  
                         for(int i = 1; i <= imp.getNChannels(); i++) {Channels[0] = "";Channels[i] = "Channel " + i;}
                         
                         String SimpleBleedThroughCorrection = "";
                         String TargetChannel = "";
-                        int SizeDilation = 5;
-                        
+                        int SizeDilation = 5;               
                         int minThreshold = 2000;
-                        double maxThreshold = Math.pow(2,imp.getBitDepth());
-                        
+                        double maxThreshold = Math.pow(2,imp.getBitDepth());  
                         int FinalObjectDiameter = 5;
                         
                         GenericDialog gd = new GenericDialog("Tracking PreProcessing v0.1");
                         gd.addMessage("Preprocessing Options:");
-                        gd.addMessage(FileInfo);
-                        
+                        gd.addMessage(FileInfo); 
                         gd.addMessage("___________________________________________");
                         gd.addChoice("Target Channel:", Channels, "Channel 2");
                         gd.addChoice("Simple Bleed Through Correction:", Channels, "Channel 1");
@@ -215,14 +182,6 @@ public class TrkPP_ implements PlugInFilter {
                         gd.addMessage("Author: Seth Winfree Indiana University   04/4/2014");
                         gd.showDialog();
                         if (gd.wasCanceled()) {return;}
-
-//                        SimpleBleedThroughCorrection = gd.getNextString();
-//                        TargetChannel = gd.getNextString();
-//                        SizeDilation = (int)gd.getNextNumber();
-//                        minThreshold = (int)gd.getNextNumber();
-//                        maxThreshold = (int)gd.getNextNumber();
-//                        FinalObjectDiameter = (int)gd.getNextNumber();
-                     
                         
                         this.start[0] = gd.getNextChoiceIndex();
                         this.start[1] = gd.getNextChoiceIndex();
@@ -233,9 +192,5 @@ public class TrkPP_ implements PlugInFilter {
                         this.start[6] = (int)gd.getNextNumber();
                         this.start[7] = (int)gd.getNextNumber();
                         this.start[8] = (int)gd.getNextNumber();
-                }
-
-
-    
-    
+                } 
 }
